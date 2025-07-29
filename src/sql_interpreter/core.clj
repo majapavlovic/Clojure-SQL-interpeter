9fad5dd28344466d254dd9e8b57e8d61d96d0881
majapavlovic <maja.pavlovic2703@gmail.com>
2025-07-30 00:43:34 +0200

    select and from interpretation

--- src/sql_interpreter/core.clj
+++ src/sql_interpreter/core.clj
@@ -1,7 +1,17 @@
 (ns sql-interpreter.core
-  (:require [sql-interpreter.data :as data]))
+  (:require [sql-interpreter.data :as data]
+            [clojure.string :as str]))
+
+
+(defn sql-query [query table-data]
+  (let [[_ select-str where-clause] (re-matches #"(?i)SELECT (.+) FROM \w+(?: WHERE (.+))?" query)
+        selected-cols (map keyword (map str/trim (str/split select-str #",")))]
+    (map #(select-keys % selected-cols) table-data)))
+
 
 (defn -main []
-  (let [data (data/load-csv-as-maps "resources/data.csv")]
-    (doseq [row data]
+  (let [table (data/load-csv-as-maps "resources/data.csv")
+        query "SELECT one, two FROM data"
+        result (sql-query query table)]
+    (doseq [row result]
       (println row))))
